e7904d0d0a3795b149b7115d06041aee783a47b6
Igor <igrik12@hotmail.com>
2020-09-19 18:03:30 +0100

    Documentation

--- ios/Classes/SwiftBatteryInfoPlugin.swift
+++ ios/Classes/SwiftBatteryInfoPlugin.swift
@@ -1,15 +1,20 @@
 import Flutter
 import UIKit
 
+//Describes the current charging state of the device battery
 enum BatteryState {
   static let charging = "charging"
   static let discharging = "discharging"
 }
 
+//Custom error code for the responce
 enum CustomFlutterErrorCode {
   static let unavailable = "UNAVAILABLE"
 }
-
+/*
+Reponsible for the retrieval of the device battery charging state and level.
+Supports both single API call and stream.
+*/
 public class SwiftBatteryInfoPlugin: NSObject, FlutterPlugin {
   let generator = BatteryInfoGenerator()
 
@@ -21,61 +26,79 @@ public class SwiftBatteryInfoPlugin: NSObject, FlutterPlugin {
     eventChannel.setStreamHandler(SwiftStreamHandler())
   }
 
+  // Handles the API call from the flutter side through the Method Channel to retrive battery information (level and charging status)
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     let batteryInfo = generator.generate()
-    let level: Int? = batteryInfo["batteryLevel"] as? Int
-    if(level! < 0){
-      result(FlutterError(code: CustomFlutterErrorCode.unavailable,
-      message: "Battery level unavailable",
-      details: nil))
+    
+    if let level = batteryInfo["batteryLevel"] as? Int{
+      if(level < 0){
+        result(FlutterError(
+          code: CustomFlutterErrorCode.unavailable,
+          message: "Battery level unavailable",
+          details: nil))
+      }
     }
-    let status: String? = batteryInfo["batteryStatus"] as? String
-    if(status == CustomFlutterErrorCode.unavailable){
-      result(FlutterError(
-        code: CustomFlutterErrorCode.unavailable,
-        message: "Battery status unavailable",
-        details: nil))
-    }
+
+    if let status = batteryInfo["batteryStatus"] as? String{
+      if(status == CustomFlutterErrorCode.unavailable){
+        result(FlutterError(
+          code: CustomFlutterErrorCode.unavailable,
+          message: "Battery status unavailable",
+          details: nil))
+      }
+    }    
     result(batteryInfo)
   }
 }
 
+/*
+Stream handler for the device battery information retrieval.
+*/
 class SwiftStreamHandler: NSObject, FlutterStreamHandler {
   let generator = BatteryInfoGenerator()
   var eventSink: FlutterEventSink?
 
+  // Entry point for the stream subscription, pushing battery events to the stream sink
   public func onListen(withArguments arguments: Any?, eventSink: @escaping FlutterEventSink) -> FlutterError? {
     self.eventSink = eventSink
     let batteryInfo = generator.generate()
-    let level: Int? = batteryInfo["batteryLevel"] as? Int
 
-    if(level! < 0){
-      eventSink(FlutterError(
-        code: CustomFlutterErrorCode.unavailable,
-        message: "Battery level unavailable",
-        details: nil))
-      return nil
+    if let level = batteryInfo["batteryLevel"] as? Int{
+      if(level < 0){
+        eventSink(FlutterError(
+          code: CustomFlutterErrorCode.unavailable,
+          message: "Battery level unavailable",
+          details: nil))
+        return nil
+      }
     }
 
-    let status: String? = batteryInfo["batteryStatus"] as? String
-    if(status == CustomFlutterErrorCode.unavailable){
-      eventSink(FlutterError(
-        code: CustomFlutterErrorCode.unavailable,
-        message: "Battery status unavailable",
-        details: nil))
-      return nil
-    }
+    if let status = batteryInfo["batteryStatus"] as? String{
+      if(status == CustomFlutterErrorCode.unavailable){
+        eventSink(FlutterError(
+          code: CustomFlutterErrorCode.unavailable,
+          message: "Battery status unavailable",
+          details: nil))
+        return nil
+      }
+    }   
 
     eventSink(batteryInfo)
     return nil
   }
 
+  /*
+  Disposal of the stream sink resources
+  */
   public func onCancel(withArguments arguments: Any?) -> FlutterError? {
     eventSink = nil
     return nil
   }
 } 
 
+/*
+  Responsible for the gereration of the battery information by calling UIDevice API 
+*/
 struct BatteryInfoGenerator{
     let device: UIDevice
 
@@ -100,7 +123,7 @@ struct BatteryInfoGenerator{
     private func getBatteryLevel() -> Int {
       Int(device.batteryLevel * 100)
     }
-
+    // Generates the battery information
     func generate() -> Dictionary<String, Any>{
       var batteryInfo: [String: Any] = [:]
       batteryInfo["batteryLevel"] = getBatteryLevel()
