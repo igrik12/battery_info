3acd0ea242352568c8164dd3eabeac817e377084
Igor <igrik12@hotmail.com>
2020-09-19 18:33:01 +0100

    Quick clean up before publish

--- lib/method_channel_battery_info.dart
+++ lib/method_channel_battery_info.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/services.dart';
-import 'package:meta/meta.dart';
 import 'battery_info_platform_interface.dart';
 
 /// An implementation of [BatteryInfoPlatform] that uses method channels.
 class MethodChannelBatteryInfo extends BatteryInfoPlatform {
   /// The method channel used to interact with the native platform.
-  @visibleForTesting
   static MethodChannel channel =
       MethodChannel('com.igrik12.battery_info/channel');
 
   /// The event channel used to interact with the native platform.
-  @visibleForTesting
   static EventChannel eventChannel =
       EventChannel('com.igrik12.battery_info/stream');
 }
