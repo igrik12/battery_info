4625dc2ac6cd6a1c762f5ba56ce658849eefcb56
Igor <igrik12@hotmail.com>
2020-09-19 14:05:35 +0100

    gitflow-feature-stash: ios

--- ios/Classes/SwiftBatteryInfoPlugin.swift
+++ ios/Classes/SwiftBatteryInfoPlugin.swift
@@ -1,67 +1,107 @@
 import Flutter
 import UIKit
 
-public class SwiftBatteryInfoPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
-  private var eventSink: FlutterEventSink?
+enum BatteryState {
+  static let chargins = "charging"
+  static let discharging = "discharging"
+}
+
+enum CustomFlutterErrorCode {
+  static let unavailable = "UNAVAILABLE"
+}
+
+
+public class SwiftBatteryInfoPlugin: NSObject, FlutterPlugin {
+  let generator = BatteryInfoGenerator()
 
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "com.igrik12.battery_info/channel", binaryMessenger: registrar.messenger())
-    
-    let eventChannel = FlutterEventChannel(name: "com.igrik12.battery_info/stream", binaryMessenger: messenger!)                                                                                
-    eventChannel.setStreamHandler(SwiftBatteryInfoPlugin())
-
     let instance = SwiftBatteryInfoPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+    let eventChannel = FlutterEventChannel(name: "com.igrik12.battery_info/stream", binaryMessenger: messenger!)                                                                                
+    eventChannel.setStreamHandler(SwiftBatteryInfoPlugin())
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    let batteryInfo = generator.generate()
+    let level: Int? = batteryInfo["batteryLevel"] as? Int
+    if(level! < 0){
+      result(FlutterError(code: CustomFlutterErrorCode.unavailable,
+      message: "Battery level unavailable",
+      details: nil))
+    }
+    let status: String? = batteryInfo["batteryStatus"] as? String
+    if(status == CustomFlutterErrorCode.unavailable){
+      result(FlutterError(code: CustomFlutterErrorCode.unavailable,
+      message: "Battery status unavailable"))
+    }
+    result(batteryInfo)
   }
+}
 
+class SwiftStreamHandler: NSObject, FlutterStreamHandler {
+  let generator = BatteryInfoGenerator()
+  var eventSink: FlutterEventSink?
+
+  
   public func onListen(withArguments arguments: Any?, eventSink: @escaping FlutterEventSink) -> FlutterError? {
-    self.eventSink = eventSink
-    UIDevice.current.isBatteryMonitoringEnabled = true
-    sendBatteryStateEvent()
-    NotificationCenter.default.addObserver(
-      self,
-      selector: #selector(AppDelegate.onBatteryStateDidChange),
-      name: UIDevice.batteryStateDidChangeNotification,
-      object: nil)
+    let batteryInfo = generator.generate()
+    let level: Int? = batteryInfo["batteryLevel"] as? Int
+
+    if(level! < 0){
+      eventSink(FlutterError(code: CustomFlutterErrorCode.unavailable,
+        message: "Battery level unavailable",
+        details: nil))
+      return nil
+    }
+
+    let status: String? = batteryInfo["batteryStatus"] as? String
+    if(status == CustomFlutterErrorCode.unavailable){
+      eventSink(FlutterError(code: CustomFlutterErrorCode.unavailable,
+        message: "Battery status unavailable",
+        details: nil))
+      return nil
+    }
+
+    result(batteryInfo)
     return nil
-  }
-
-  private func receiveBatteryLevel(result: FlutterResult) {
-    let device = UIDevice.current
-    device.isBatteryMonitoringEnabled = true
-    guard device.batteryState != .unknown  else {
-      result(FlutterError(code: MyFlutterErrorCode.unavailable,
-                          message: "Battery info unavailable",
-                          details: nil))
-      return
-    }
-    result(Int(device.batteryLevel * 100))
-  }
-
-  private func sendBatteryStateEvent() {
-    guard let eventSink = eventSink else {
-      return
-    }
-
-    switch UIDevice.current.batteryState {
-    case .full:
-      eventSink(BatteryState.charging)
-    case .charging:
-      eventSink(BatteryState.charging)
-    case .unplugged:
-      eventSink(BatteryState.discharging)
-    default:
-      eventSink(FlutterError(code: MyFlutterErrorCode.unavailable, message: "Charging status unavailable", details: nil))
-    }
   }
 
   public func onCancel(withArguments arguments: Any?) -> FlutterError? {
-    NotificationCenter.default.removeObserver(self)
     eventSink = nil
     return nil
   }
+} 
+
+struct BatteryInfoGenerator{
+    let device: UIDevice
+
+    init(){
+      device = UIDevice.current
+      device.isBatteryMonitoringEnabled = true
+    }
+
+    private func sendBatteryState() {
+      switch UIDevice.current.batteryState {
+        case .full:
+          return BatteryState.charging
+        case .charging:
+          return BatteryState.charging
+        case .unplugged:
+          return BatteryState.discharging
+        default:
+          return CustomFlutterErrorCode.unavailable
+        }
+    }
+
+    private func getBatteryLevel() {
+      Int(device.batteryLevel * 100)
+    }
+
+    func generate() -> Dictionary<String, Any>{
+      var batteryInfo: [String: Any] = [:]
+      batteryInfo["batteryLevel"] = getBatteryLevel()
+      batteryInfo["batteryStatus"] = getBatteryStatus()
+      return batteryInfo
+    }
 }
