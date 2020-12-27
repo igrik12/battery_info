9ad5bce42d5ae4d653c49dfbad884d59ff67d2dd
Igor <igrik12@hotmail.com>
2020-12-27 10:51:38 +0000

    gitflow-hotfix-stash: platform-interface
    - Properly using platform interface for the API

--- lib/battery_info_plugin.dart
+++ lib/battery_info_plugin.dart
@@ -1,64 +1,25 @@
 import 'dart:async';
+import 'package:battery_info/battery_info_platform_interface.dart';
 import 'package:battery_info/model/android_battery_info.dart';
 import 'package:battery_info/model/iso_battery_info.dart';
-import 'package:flutter/services.dart';
 
 /// Plugin for accessing the battery information of the device
 class BatteryInfoPlugin {
-  static const MethodChannel _methodChannel =
-      const MethodChannel('com.igrik12.battery_info/channel');
-  static const EventChannel _streamChannel =
-      EventChannel("com.igrik12.battery_info/stream");
-
   /// Returns the battery info as a single API call
-  Future<AndroidBatteryInfo> get androidBatteryInfo async {
-    try {
-      final batteryInfo = await _methodChannel.invokeMethod('getBatteryInfo');
-      final converted = AndroidBatteryInfo.fromJson(Map.from(batteryInfo));
-      return converted;
-    } on PlatformException catch (e) {
-      print(e.message);
-      return null;
-    }
-  }
+  Future<AndroidBatteryInfo> get androidBatteryInfo async =>
+      await BatteryInfoPlatform.instance.androidBatteryInfo();
 
   /// Returns a stream of [BatteryInfo] data that is pushed out to the
   /// subscribers on updates
-  Stream<AndroidBatteryInfo> get androidBatteryInfoStream {
-    return _streamChannel.receiveBroadcastStream().map((data) {
-      try {
-        final converted = AndroidBatteryInfo.fromJson(Map.from(data));
-        return converted;
-      } on PlatformException catch (e) {
-        print(e.message);
-        return null;
-      }
-    });
-  }
+  Stream<AndroidBatteryInfo> get androidBatteryInfoStream =>
+      BatteryInfoPlatform.instance.androidBatteryInfoStream();
 
   /// Returns the battery info as a single API call
-  Future<IosBatteryInfo> get iosBatteryInfo async {
-    try {
-      final batteryInfo = await _methodChannel.invokeMethod('getBatteryInfo');
-      final converted = IosBatteryInfo.fromJson(Map.from(batteryInfo));
-      return converted;
-    } on PlatformException catch (e) {
-      print(e.message);
-      return null;
-    }
-  }
+  Future<IosBatteryInfo> get iosBatteryInfo async =>
+      BatteryInfoPlatform.instance.iosBatteryInfo();
 
   /// Returns a stream of [IsoBatteryInfo] data that is pushed out to the
   /// subscribers on updates
-  Stream<IosBatteryInfo> get iosBatteryInfoStream {
-    return _streamChannel.receiveBroadcastStream().map((data) {
-      try {
-        final converted = IosBatteryInfo.fromJson(Map.from(data));
-        return converted;
-      } on PlatformException catch (e) {
-        print(e.message);
-        return null;
-      }
-    });
-  }
+  Stream<IosBatteryInfo> get iosBatteryInfoStream =>
+      BatteryInfoPlatform.instance.iosBatteryInfoStream();
 }
--- lib/method_channel_battery_info.dart
+++ lib/method_channel_battery_info.dart
@@ -1,13 +1,65 @@
+import 'package:battery_info/model/android_battery_info.dart';
+import 'package:battery_info/model/iso_battery_info.dart';
 import 'package:flutter/services.dart';
 import 'battery_info_platform_interface.dart';
 
 /// An implementation of [BatteryInfoPlatform] that uses method channels.
-class MethodChannelBatteryInfo extends BatteryInfoPlatform {
+class MethodChannelBatteryInfo implements BatteryInfoPlatform {
   /// The method channel used to interact with the native platform.
-  static MethodChannel channel =
+  static MethodChannel _methodChannel =
       MethodChannel('com.igrik12.battery_info/channel');
 
   /// The event channel used to interact with the native platform.
-  static EventChannel eventChannel =
+  static EventChannel _streamChannel =
       EventChannel('com.igrik12.battery_info/stream');
+
+  @override
+  Future<AndroidBatteryInfo> androidBatteryInfo() async {
+    try {
+      final batteryInfo = await _methodChannel.invokeMethod('getBatteryInfo');
+      final converted = AndroidBatteryInfo.fromJson(Map.from(batteryInfo));
+      return converted;
+    } on PlatformException catch (e) {
+      print(e.message);
+      return null;
+    }
+  }
+
+  @override
+  Future<IosBatteryInfo> iosBatteryInfo() async {
+    try {
+      final batteryInfo = await _methodChannel.invokeMethod('getBatteryInfo');
+      final converted = IosBatteryInfo.fromJson(Map.from(batteryInfo));
+      return converted;
+    } on PlatformException catch (e) {
+      print(e.message);
+      return null;
+    }
+  }
+
+  @override
+  Stream<AndroidBatteryInfo> androidBatteryInfoStream() {
+    return _streamChannel.receiveBroadcastStream().map((data) {
+      try {
+        final converted = AndroidBatteryInfo.fromJson(Map.from(data));
+        return converted;
+      } on PlatformException catch (e) {
+        print(e.message);
+        return null;
+      }
+    });
+  }
+
+  @override
+  Stream<IosBatteryInfo> iosBatteryInfoStream() {
+    return _streamChannel.receiveBroadcastStream().map((data) {
+      try {
+        final converted = IosBatteryInfo.fromJson(Map.from(data));
+        return converted;
+      } on PlatformException catch (e) {
+        print(e.message);
+        return null;
+      }
+    });
+  }
 }
